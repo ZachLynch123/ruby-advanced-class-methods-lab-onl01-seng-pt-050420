ff2773924438253a78145ed5cc96f013e90e84c1
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-24 22:01:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,48 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  
+  
+  def self.create
+    song = Song.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(query)
+    self.all.find{|song|
+      song.name == query
+    }
+    
+  end
+  
+  def self.find_or_create_by_name(query)
+    if self.all.any?{|song| song.name == query}
+      self.find_by_name(query)
+    else
+      self.create_by_name(query)
+    end
+  end
+  
+  
+  def self.alphabetical
+    
+  end
 
   def self.all
     @@all
